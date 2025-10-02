b28e3bdf8ce37adeaecb5e129ef2469db2631379
Amanbol <kanomru@gmail.com>
2025-10-02 23:28:28 +0500

    Python classes

--- python_classes.py
+++ python_classes.py
@@ -83,7 +83,7 @@ acct.withdraw(-10)
 def is_prime(n):
     if n < 2:
         return False
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(2, n):
         if n % i == 0:
             return False
     return True
